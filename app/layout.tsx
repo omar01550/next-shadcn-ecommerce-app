b16c02213cacb6f0478588cb94cad01a15df758c
omar01550 <esmaelalhabiby@gmail.com>
2024-02-09 22:23:17 +0200

    remove-scroll-top-btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 import Header from './components/header'
 import Footer from './components/footer'
 import { ThemeWrapper } from './context/theme'
-// import SideBar from './components/SideBar'
-import ScrollToTop from './components/scrollToTop'
+import SideBar from './components/SideBar'
+// import ScrollToTop from './components/scrollToTop'
 
 const Layout = ({ children}) => {
 
