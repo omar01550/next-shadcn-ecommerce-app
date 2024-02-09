42b4f68b8072198334760d61c8590c7e35284bb9
omar01550 <esmaelalhabiby@gmail.com>
2024-02-09 22:37:03 +0200

    remove unesery files

--- app/api/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-
-export function GET(request:Request) {
-    console.log('get requets logout');
-    
-    return NextResponse.redirect(new URL('/dashboard',request.url))
-}
